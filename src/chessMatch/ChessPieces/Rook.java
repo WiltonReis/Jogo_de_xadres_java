d4d1f832cbf910578153bc71d66eb15ea61080e8
Wilton Reis <wiltonfilho0825@gmail.com>
2025-08-19 21:24:39 -0300

    feat: Adiciona a lógica de manipulação de peças no tabuleiro

--- src/chessMatch/ChessPieces/Rook.java
+++ src/chessMatch/ChessPieces/Rook.java
@@ -1,4 +1,71 @@
 package chessMatch.ChessPieces;
 
-public class Rook {
+import chessMatch.Board;
+import chessMatch.Color;
+import chessMatch.Piece;
+import chessMatch.Position;
+
+public class Rook extends Piece {
+
+    public Rook(Color color, Board board) {
+        super(color, board);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] moves = new boolean[8][8];
+
+        Position p = new Position(0, 0);
+
+        // above
+        p.setValues(position.getRow() - 1, position.getColumn());
+        while (board.positionExists(p) && !board.thereIsAPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() - 1);
+        }
+        if (board.positionExists(p) && isThereOpponentPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+        }
+
+        // left
+        p.setValues(position.getRow(), position.getColumn() - 1);
+        while (board.positionExists(p) && !board.thereIsAPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() - 1);
+        }
+        if (board.positionExists(p) && isThereOpponentPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+        }
+
+        // right
+        p.setValues(position.getRow(), position.getColumn() + 1);
+        while (board.positionExists(p) && !board.thereIsAPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+            p.setColumn(p.getColumn() + 1);
+        }
+        if (board.positionExists(p) && isThereOpponentPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+        }
+
+        // below
+        p.setValues(position.getRow() + 1, position.getColumn());
+        while (board.positionExists(p) && !board.thereIsAPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+            p.setRow(p.getRow() + 1);
+        }
+        if (board.positionExists(p) && isThereOpponentPiece(p)) {
+            moves[p.getRow()][p.getColumn()] = true;
+        }
+
+        return moves;
+    }
+
+    @Override
+    public String toString() {
+        if (color == Color.WHITE) {
+            return "whiteRook";
+        } else {
+            return "blackRook";
+        }
+    }
 }
--- src/chessMatch/Piece.java
+++ src/chessMatch/Piece.java
@@ -2,11 +2,11 @@ package chessMatch;
 
 public abstract class Piece {
 
-    private Color color;
-    private Position position;
-    private Board board;
+    protected Color color;
+    protected Position position;
+    protected Board board;
 
-    public Piece(Color color, Position position, Board board) {
+    public Piece(Color color, Board board) {
         this.color = color;
         this.board = board;
     }
@@ -19,5 +19,21 @@ public abstract class Piece {
         return position;
     }
 
-    public abstract boolean[][] possibleMove();
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereOpponentPiece(Position position) {
+        return board.piece(position).getColor() != color;
+    }
 }
